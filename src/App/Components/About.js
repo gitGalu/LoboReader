8e2db664e7e78248f1d6c3d336990c507134dbe2
gitGalu <galu83@gmail.com>
2021-04-21 14:51:59 +0200

    Reading list CSV export feature

--- src/App/Components/About.js
+++ src/App/Components/About.js
@@ -1,4 +1,6 @@
+import React, { useState, useEffect } from 'react';
 import preval from 'preval.macro';
+import { CSVLink } from "react-csv";
 import {
     Modal,
     ModalHeader,
@@ -8,11 +10,39 @@ import {
     ROLE,
     SIZE
 } from 'baseui/modal';
-
+import { StyledLink } from "baseui/link";
 import { Link } from 'react-router-dom';
+import db from '../Components/Db';
 
 const About = (props) => {
     const dateTimeStamp = preval`module.exports = new Date().toLocaleString();`
+
+    const csvLinkEl = React.useRef();
+    const [data, setData] = useState([]);
+
+    const headers = [
+        { label: "identifier", key: "id" },
+        { label: "title", key: "title" },
+        { label: "page", key: "page" },
+        { label: "archived", key: "archived" },
+        { label: "read", key: "read" }
+    ];
+
+    const downloadReport = async () => {
+        db.collection
+            .toArray()
+            .then((items) => {
+                setData(items);
+            });
+    }
+
+    useEffect(() => {
+        if (csvLinkEl.current != undefined) {
+            setTimeout(() => {
+                csvLinkEl.current.link.click();
+            });
+        }
+    }, [data]);
 
     return (
         <Modal
@@ -26,11 +56,13 @@ const About = (props) => {
             <ModalHeader>LoboReader<br />for Internet Archive</ModalHeader>
             <ModalBody>
                 Build date: {dateTimeStamp}<br /><br />
-                LoboReader is an unofficial reader for the Internet Archive "Magazine Rack".<br/><br/>
+                LoboReader is an unofficial reader for the Internet Archive "Magazine Rack".<br /><br />
+                <StyledLink animateUnderline onClick={downloadReport}>Click here</StyledLink> to download your reading list.<br /><br />
+                <CSVLink headers={headers} data={data} ref={csvLinkEl} filename="LoboReader-export.csv"></CSVLink>
                 For changelog, docs &amp; licensing please visit project page on GitHub.
             </ModalBody>
             <ModalFooter>
-            <Link to={{ pathname: "https://github.com/gitGalu/LoboReader" }} target="_blank"><ModalButton kind="primiary">Visit GitHub</ModalButton></Link>
+                <Link to={{ pathname: "https://github.com/gitGalu/LoboReader" }} target="_blank"><ModalButton kind="primiary">Visit GitHub</ModalButton></Link>
             </ModalFooter>
         </Modal>
     )
