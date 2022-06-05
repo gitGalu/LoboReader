a3e2db472dcaf6229f35b13052500e5642d5e164
gitGalu <galu83@gmail.com>
2022-06-05 23:19:40 +0200

    Collection view: display toggle (grid / list)

--- src/App/Routes/Collection.js
+++ src/App/Routes/Collection.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { useHistory } from "react-router-dom";
-import ItemMetadataListItem from '../Components/ItemMetadataListItem'
 import { Spinner } from 'baseui/spinner';
+import { Button, KIND, SIZE } from 'baseui/button'
 import { Centered } from '../Components/Centered';
 import db from '../Components/Db';
 import ItemDrawer from '../Components/ItemDrawer';
+import ItemMetadataListItem from '../Components/ItemMetadataListItem';
 import { Masonry } from 'masonic';
 
 const Collection = (props) => {
@@ -13,7 +14,9 @@ const Collection = (props) => {
 
   const [browserItems, setBrowserItems] = useState([]);
   const [initial, setInitial] = useState(true);
-  const [gridView, setGridView] = useState(false);
+  const [gridView, setGridView] = useState(
+    JSON.parse(localStorage.getItem('collection.gridView')) || false
+  );
 
   useEffect(() => {
     reloadDb();
@@ -45,7 +48,7 @@ const Collection = (props) => {
   }
 
   const findIndex = (identifier) => {
-    var ret = -1;
+    let ret = -1;
     browserItems.forEach((element, index) => {
       if (element.id === identifier) {
         ret = index;
@@ -58,7 +61,7 @@ const Collection = (props) => {
     db.collection.update({ id: identifier }, { archived: true })
       .then((result) => {
         drawer.current.hideDrawer();
-        var index = findIndex(identifier);
+        let index = findIndex(identifier);
         browserItems[index].disabled = true;
         setBrowserItems([]);
         setBrowserItems(browserItems);
@@ -80,11 +83,11 @@ const Collection = (props) => {
 
   const renderData = () => {
     return (
-      <div style={{ paddingRight: '16px' }}>
+      <div style={{ paddingTop: gridView ? '12px' : '0px', paddingRight: '16px' }}>
         <Masonry
           items={browserItems}
           columnGutter={gridView ? 16 : 0}
-          columnWidth={80}
+          columnWidth={100}
           columnCount={gridView ? undefined : 1}
           overscanBy={4}
           render={DataItem}
@@ -124,8 +127,26 @@ const Collection = (props) => {
         }}
       />
 
-      <div style={{ marginTop: '4px', marginRight: '14px', fontSize: '85%', color: '#cbcbcb' }}>
-        Your Collection
+      <div style={{ fontSize: '85%', paddingTop: '0px', paddingBottom: '32px', color: '#cbcbcb' }}>
+        <div style={{ float: 'left', paddingTop: '6px' }}>Your Collection</div>
+        <span style={{ float: 'right', paddingRight: '16px' }}>
+          <Button
+            size={SIZE.mini}
+            kind={KIND.tertiary}
+            onClick={() => {
+              setGridView(!gridView);
+              localStorage.setItem('collection.gridView',  JSON.stringify(!gridView));
+            }}
+            overrides={{
+              Root: {
+                style: ({ $theme }) => ({
+                  backgroundColor:'rgb(246, 246, 246)',
+                  width:'96px'
+                })
+              }
+            }}
+          >{gridView ? "Grid View" : "List View"}</Button>
+        </span>
       </div>
 
       {(browserItems.length > 0 && !initial)
